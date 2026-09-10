20b0ef60763ca45f79a0bb80932b43e5a1ebd46d
siwraaj-siwraaj <siwraaj@gmail.com>
2026-09-10 10:24:07 +0530

    Install AndroidX splash screen before Capacitor startup

--- src/frontend/android/app/src/main/java/com/siwraaj/attendance/MainActivity.java
+++ src/frontend/android/app/src/main/java/com/siwraaj/attendance/MainActivity.java
@@ -1,5 +1,16 @@
 package com.siwraaj.attendance;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.core.splashscreen.SplashScreen;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+    }
+}
