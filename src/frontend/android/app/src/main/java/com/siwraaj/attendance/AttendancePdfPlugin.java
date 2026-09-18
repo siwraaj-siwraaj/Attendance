d8bb96840ab803a129cdab9052f302a2cdcbb943
siwraaj-siwraaj <siwraaj@gmail.com>
2026-09-18 08:16:07 +0530

    Fix Capacitor PluginMethod import

--- src/frontend/android/app/src/main/java/com/siwraaj/attendance/AttendancePdfPlugin.java
+++ src/frontend/android/app/src/main/java/com/siwraaj/attendance/AttendancePdfPlugin.java
@@ -21,7 +21,7 @@ import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
+import com.getcapacitor.PluginMethod;
 
 import java.io.File;
 import java.io.FileInputStream;
